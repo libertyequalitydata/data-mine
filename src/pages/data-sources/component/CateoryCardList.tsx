52bc5cfa24011d1b7b003fadda09e4487491b98a
polymahh <otman.elkantaoui@gmail.com>
2022-11-28 21:55:54 +0100

    changed icons folder/ dynamacly imported added design elements to connector card added no results found / loading / similar sources

--- src/pages/data-sources/component/CateoryCardList.tsx
+++ src/pages/data-sources/component/CateoryCardList.tsx
@@ -1,8 +1,9 @@
 import CategoryContainner from "./CategoryContainner";
-import { VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 import { useEffect, useContext } from "react";
 import DataContext from "../../../contexts/DataContext";
 import RequestDataSources from "./RequestDataSources";
+import SimilarDataSources from "./SimilarDataSources";
 
 const CateoryCardList = () => {
   const { categories } = useContext(DataContext);
@@ -21,8 +22,21 @@ const CateoryCardList = () => {
                 <CategoryContainner category={category} />
               )
           )}
+        {categories && categories.every((cat: any) => cat.items.length === 0) && (
+          <Text color={"whiteText"} fontSize={"24px"} fontWeight={600}>
+            No Results Found
+          </Text>
+        )}
+        {!categories && (
+          <Text color={"whiteText"} fontSize={"24px"} fontWeight={600}>
+            Loading ...
+          </Text>
+        )}
       </VStack>
       <RequestDataSources />
+      {categories && categories.every((cat: any) => cat.items.length === 0) && (
+        <SimilarDataSources />
+      )}
     </>
   );
 };
